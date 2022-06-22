84aa077c409fc15f7356e2a5d6e54808d8ca6b4d
Rodrigo Antonio Gómez Hernández <70409277+rodrigo-gomez@users.noreply.github.com>
2022-06-22 08:01:19 -0500

    Avances

--- Retail_Dashboard/ui.R
+++ Retail_Dashboard/ui.R
@@ -1,13 +1,14 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+########################################################################
+########### DASHBOARD OPORTUNIDADES DE ENTREGA SECTOR RETAIL ###########
+########################################################################
+## Elaborado por: Sergio Castellanos, Sebstián Laverde, Rodrigo Gómez ##
+########################################################################
+############## Presentado el 2022-06-25 a Stalin Guerrero ##############
+########################################################################
+
 
 library(shiny)
+
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
