3ad7ecbfe1affca6eb1981ef378592f52b04fa91
rodrigo-gomez <terranautas@hotmail.com>
2022-06-21 23:50:13 -0500

    Se Crean los cuatro Tabs (Páneles) para los objetivos plantedos

--- Retail_Dashboard/server.R
+++ Retail_Dashboard/server.R
@@ -22,5 +22,35 @@ shinyServer(function(input, output) {
         hist(x, breaks = bins, col = 'darkgray', border = 'white')
 
     })
+    output$distPlot2 <- renderPlot({
+      
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2]
+      bins <- seq(min(x), max(x), length.out = input$bins2 + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      
+    })
+    output$distPlot3 <- renderPlot({
+      
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2]
+      bins <- seq(min(x), max(x), length.out = input$bins3 + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      
+    })
+    output$distPlot4 <- renderPlot({
+      
+      # generate bins based on input$bins from ui.R
+      x    <- faithful[, 2]
+      bins <- seq(min(x), max(x), length.out = input$bins4 + 1)
+      
+      # draw the histogram with the specified number of bins
+      hist(x, breaks = bins, col = 'darkgray', border = 'white')
+      
+    })
 
 })
--- Retail_Dashboard/ui.R
+++ Retail_Dashboard/ui.R
@@ -13,10 +13,11 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Dashboard Oportunidad de Entrega Sector Retail"),
 
     # Sidebar with a slider input for number of bins
-    sidebarLayout(
+    tabsetPanel(tabPanel("Descriptivos",
+      sidebarLayout(
         sidebarPanel(
             sliderInput("bins",
                         "Number of bins:",
@@ -29,5 +30,52 @@ shinyUI(fluidPage(
         mainPanel(
             plotOutput("distPlot")
         )
+    )),
+    tabPanel("Proporciones",
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput("bins2",
+                             "Number of bins:",
+                             min = 1,
+                             max = 50,
+                             value = 30)
+               ),
+               
+               # Show a plot of the generated distribution
+               mainPanel(
+                 plotOutput("distPlot2")
+               )
+    )),
+    tabPanel("Sentimientos",
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput("bins3",
+                             "Number of bins:",
+                             min = 1,
+                             max = 100,
+                             value = 50)
+               ),
+               
+               # Show a plot of the generated distribution
+               mainPanel(
+                 plotOutput("distPlot3")
+               )
+    )),
+    tabPanel("Geoespacial",
+             sidebarLayout(
+               sidebarPanel(
+                 sliderInput("bins4",
+                             "Number of bins:",
+                             min = 1,
+                             max = 100,
+                             value = 50)
+               ),
+               
+               # Show a plot of the generated distribution
+               mainPanel(
+                 plotOutput("distPlot4")
+               )
+    ))
+    
     )
 ))
